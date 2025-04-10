79e5e02ac8124f1e92466cb18c1b19d5746a0d5d
izzy <mista.rol@gmail.com>
2025-04-11 01:20:28 +0700

    Synthetic Pak corpus.

--- playground/test.js
+++ playground/test.js
@@ -1,11 +1,11 @@
-import { MarkovModel } from '../src/MarkovModel.js';
-import path from 'path';
-import fs from 'fs/promises';
-import { fileURLToPath } from 'url';
+import { MarkovModel } from "../src/MarkovModel.js";
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const modelPath = 'models/Pak-test-model.json';
-const datasetPath = 'dataset/Pak.txt';
+const modelPath = "models/Pak-synthetic-0411.json";
+const datasetPath = "dataset/Synthetic_Pak_0410.txt";
 
 /**
  * Example training function
@@ -13,22 +13,37 @@ const datasetPath = 'dataset/Pak.txt';
 async function trainModel() {
   // Create a new model
   const markov = new MarkovModel({
-    order: 4,
-    tokenType: 'word',
-    stopTokens: ['.', '!', '?']
+    order: 3,
+    tokenType: "word",
+    stopTokens: [".", "!", "?"],
   });
 
   // Import txt file for training
-  const trainingData = await fs.readFile(path.join(__dirname, datasetPath), 'utf8');
+  const trainingData = await fs.readFile(
+    path.join(__dirname, datasetPath),
+    "utf8"
+  );
+
+  // Import multiple txt files for training
+  const datasetDirectory = "dataset";
+  const datasetFiles = [
+    "Synthetic_Pak_0410.txt",
+    "Revelation_excerpts.txt",
+  ];
+  const trainingDataConcat = await Promise.all(
+    datasetFiles.map((file) =>
+      fs.readFile(path.join(__dirname, datasetDirectory, file), "utf8")
+    )
+  ).then((files) => files.join("\n"));
 
   // Train the model
-  console.log('Training model...');
-  await markov.train(trainingData);
+  console.log("Training model...");
+  await markov.train(trainingDataConcat);
 
   // Save the trained model
-  console.log('Training complete, saving model...');
+  console.log("Training complete, saving model...");
   await markov.save(path.join(__dirname, modelPath));
-  console.log('Model saved successfully');
+  console.log("Model saved successfully");
 }
 
 /**
@@ -41,8 +56,8 @@ async function generateText() {
   const generatedText = markov.generate({
     maxLength: 100,
     temperature: 0.8,
-    stopProbability: 0.69,
-    multipleSentenceProbability: 0.89
+    stopProbability: 0.5,
+    multipleSentenceProbability: 0.95,
   });
 
   console.log(generatedText);
@@ -50,4 +65,4 @@ async function generateText() {
 
 // Uncomment to run specific examples
 // await trainModel();
-await generateText(); 
+await generateText();
